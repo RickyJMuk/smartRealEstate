31d8e0934c826acf635cfdc3a91f10f811df87d9
Muasa Jnr <116666398+Muasajnr@users.noreply.github.com>
2024-01-14 19:21:25 +0300

    Update Web3.js

--- Web3.js
+++ Web3.js
@@ -1,7 +1,7 @@
 const Web3 = require("web3");
-const { abi, evm } = require("./MyToken.json"); // Assuming you compile your contract
+const { abi, evm } = require("./MyToken.json"); 
 
-const web3 = new Web3("YOUR_INFURA_ENDPOINT"); // Replace with your Infura endpoint
+const web3 = new Web3("YOUR_INFURA_ENDPOINT"); 
 
 const contractAddress = "0xd9260a069B666A052690c2b70435Cbe4a3439C76";
 const realEstateContract = new web3.eth.Contract(abi, contractAddress);
